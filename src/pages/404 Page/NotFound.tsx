d89ac1ca6322c2a0da9abf85b7b4b9e01d41a94a
oluwafisayomi <wrld26993@gmail.com>
2022-12-23 22:43:34 +0100

    [feat] added 404 page

---
+++ src/pages/404 Page/NotFound.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../404 Page/NotFound.css';
+
+function NotFound() {
+  return (
+    <>
+      <div className="cover">
+        <div className="container">
+          <h3>404</h3>
+          <div className="infoandcalltoaction">
+            <span>This Page cannot be found</span>
+            <p>
+              <NavLink to={'/'}>
+                Go back Home
+              </NavLink>
+            </p>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <p>Looks like you're lost</p>
+      </footer>
+    </>
+  )
+}
+
+export default NotFound;
