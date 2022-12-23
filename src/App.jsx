3adeb5bbc3c39dcac325f06c2214817bdf75c419
oluwafisayomi <wrld26993@gmail.com>
2022-12-23 21:05:58 +0100

    [feat] Top rated Movies ui and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import '../src/styles/media.query.css';
 import Banner from './components/banner/Banner';
 import PopularMovies from './components/movies/PopularMovies';
 import Populartv from './components/movies/Populartv';
+import TopratedMovies from './components/movies/TopratedMovies';
 import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Banner />
           <PopularMovies/>
           <Populartv/>
+          <TopratedMovies/>
         </main>
       </div>
     </>
